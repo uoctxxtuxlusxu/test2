188f8cdc445bc1dd79ab4f3d9c7d1fb6f5ff03de
uoctxxtuxlusxu <151342152+uoctxxtuxlusxu@users.noreply.github.com>
2023-11-22 22:44:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$a=10
+$b=20
+echo($a+$b)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hello world
+</body>
+</html>
